b20969d6bbb66445bae5bdde02911878b5763a3e
augusto-moura <augusto.borgesm@gmail.com>
2017-06-26 10:49:13 -0300

    Mapeada situação de Requisição de mudança de status

--- grails-app/domain/zg/augusto/RequisicaoAlteracaoRegistro.groovy
+++ grails-app/domain/zg/augusto/RequisicaoAlteracaoRegistro.groovy
@@ -7,15 +7,23 @@ class RequisicaoAlteracaoRegistro {
     static mapping = {
         table 'REQUISICOES_ALTERACAO_REGISTRO'
         version false
+
+        justificativa type: 'text'
     }
 
     static constraints = {
         dataMudanca nullable: false
         justificativa blank: false
+        status nullable: false
     }
 
     String justificativa
     Registro registro
     Date dataMudanca
+    StatusSolicitacao status = StatusSolicitacao.PENDENTE
+
+    static enum StatusSolicitacao {
+        PENDENTE, RECUSADA, ACEITA
+    }
 
 }
